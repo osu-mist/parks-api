f297b33d7e4353e105efdd6d4d3299dbe0ec7fc0
David Kalcic <kalcicd@oregonstate.edu>
2019-08-28 12:33:47 -0700

    Implement GET /owners/{ownerId}

--- api/v1/db/oracledb/owners-dao.js
+++ api/v1/db/oracledb/owners-dao.js
@@ -1,8 +1,10 @@
 const appRoot = require('app-root-path');
+const _ = require('lodash');
 
 const conn = appRoot.require('api/v1/db/oracledb/connection');
-const { serializeOwners } = require('../../serializers/owners-serializer');
+const { serializeOwner, serializeOwners } = require('../../serializers/owners-serializer');
 
+const sqlSnippet = 'SELECT ID AS "id", NAME AS "ownerName" FROM OWNERS WHERE 1 = 1';
 /**
  * @summary Get owners
  * @param {object} queries park body object
@@ -10,12 +12,39 @@ const { serializeOwners } = require('../../serializers/owners-serializer');
  *                            is not found
  */
 const getOwners = async (queries) => {
-  const sqlQuery = 'SELECT ID AS "id", NAME AS "ownerName" FROM OWNERS';
+  const sqlQuery = sqlSnippet;
   const connection = await conn.getConnection();
   try {
     const { rows } = await connection.execute(sqlQuery);
     const serializedParks = serializeOwners(rows, queries);
     return serializedParks;
+  } finally {
+    connection.close();
+  }
+};
+
+/**
+ * @param {string} id Unique park ID
+ * @returns {Promise<object>} Promise object represents a specific park or return undefined if term
+ *                            is not found
+ */
+const getOwnerById = async (id) => {
+  const sqlParams = {
+    ownerId: id,
+  };
+  const sqlQuery = `${sqlSnippet} AND ID = :ownerId`;
+  const connection = await conn.getConnection();
+  try {
+    const { rows } = await connection.execute(sqlQuery, sqlParams);
+    if (_.isEmpty(rows)) {
+      return undefined;
+    }
+    if (rows.length > 1) {
+      throw new Error('Expect a single object but got multiple results.');
+    } else {
+      const serializedPark = serializeOwner(rows[0]);
+      return serializedPark;
+    }
   } finally {
     connection.close();
   }
@@ -30,4 +59,4 @@ const getOwners = async (queries) => {
 const postOwner = async ownerBody => ownerBody;
 
 
-module.exports = { getOwners, postOwner };
+module.exports = { getOwners, getOwnerById, postOwner };
