e59af76dc32ae971292d854c836a86ce6aadcbf7
David Kalcic <kalcicd@oregonstate.edu>
2019-08-29 11:57:55 -0700

    Implement PATCH /owners/{ownerId}

--- api/v1/db/oracledb/owners-dao.js
+++ api/v1/db/oracledb/owners-dao.js
@@ -58,5 +58,34 @@ const getOwnerById = async (id) => {
  */
 const postOwner = async ownerBody => ownerBody;
 
+/**
+ * @summary Patch owner by ID
+ * @param {string} id The id of the owner to be patched
+ * @param {object} ownerBody owner body object with fields to be patched
+ * @returns {Promise<object>} Promise object represents a specific park or return undefined if term
+ *                            is not found
+ */
+const patchOwnerById = async (id, ownerBody) => {
+  const { attributes } = ownerBody.data;
+  const sqlBinds = {
+    ownerId: id,
+    ownerName: attributes.name,
+  };
+  const sqlQuery = 'UPDATE OWNERS SET NAME = :ownerName WHERE ID = :ownerID';
+  const connection = await conn.getConnection();
+  try {
+    if (_.isEmpty(attributes)) return getOwnerById(id);
+    const rawOwner = await connection.execute(sqlQuery, sqlBinds, { autoCommit: true });
+    if (rawOwner.rowsAffected === 0) return undefined;
+    return getOwnerById(id);
+  } finally {
+    connection.close();
+  }
+};
 
-module.exports = { getOwners, getOwnerById, postOwner };
+module.exports = {
+  getOwners,
+  getOwnerById,
+  postOwner,
+  patchOwnerById,
+};
